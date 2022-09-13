00ec525a5963151671b43d89daf5d2672e7eabda
Sean Marcia <sean.p.marcia@uscis.dhs.gov>
2022-09-13 11:25:53 -0400

    upate

--- packages/registry/src/components/SdgFilter.js
+++ packages/registry/src/components/SdgFilter.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 export function SdgFilter({ nominees, displayNominees, setDisplayNominees }) {
   const sdgs = ["1. No Poverty", "2. Zero Hunger", "3. Good Health and Well-being", "4. Quality Education", "5. Gender Equality", "6. Clean Water and Sanitation", "7. Affordable and Clean Energy", "8. Decent Work and Economic Growth", "9. Industry, Innovation and Infrastructure", "10. Reduced Inequality", "11. Sustainable Cities and Communities", "12. Responsible Consumption and Production", "13. Climate Action", "14. Life Below Water", "15. Life on Land", "16. Peace and Justice Strong Institutions", "17. Partnerships to achieve the Goal"]
 
+  
   const [checkedState, setCheckedState] = useState(
     Array.from({ length: sdgs.length }, (_, i) => i + 1)
   );
